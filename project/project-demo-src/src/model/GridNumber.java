ccf14fd0201b2ee2f7ae5d958e60ce5d3a3550ef
MesonLu <1613368942@qq.com>
2024-05-05 17:30:39 +0800

    搭建了四个方向的移动框架，具体需要在gridnumber中实现，添加了每一步生成随机位置一个2或4的方法addRandomNumber

--- project/project-demo-src/src/model/GridNumber.java
+++ project/project-demo-src/src/model/GridNumber.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -14,7 +15,7 @@ public class GridNumber {
     public GridNumber(int xCount, int yCount) {
         this.X_COUNT = xCount;
         this.Y_COUNT = yCount;
-        this.numbers = new int[this.X_COUNT][this.Y_COUNT];
+        this.numbers = new int[this.X_COUNT][this.Y_COUNT];//面板大小如4x4，5x5，6x6等
         this.initialNumbers();
     }
 
@@ -53,9 +54,65 @@ public class GridNumber {
 
     //todo: finish the method of four direction moving.
     public void moveRight() {
+        //滑动和合并：当玩家滑动网格时，所有瓷砖会向所选方向移动，直到它们撞到边缘或另一个不可移动的瓷砖。
+        //如果两个相邻的瓷砖编号相同，它们将在停止移动后合并为一个瓷砖，其数值等于它们值的总和，
+        //并且这个新合并的瓷砖也将沿着移动方向继续移动，直到不能再移动。
+        //如果存在三个相邻的相同值的瓷砖，滑动方向末端的两个块将合并在一起。
+        //最后一个位置的索引
+        int indexX_last = X_COUNT - 1;
+        int indexY_last = Y_COUNT - 1;
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i][1] += numbers[i][0];
-            numbers[i][0] = 0;
+            ArrayList<Integer> list = new ArrayList<>();
+            ArrayList<Integer> y = new ArrayList<>();
+            for (int j = 0; j < numbers[i].length; j++) {
+                if (numbers[i][j] != 0) {
+                    list.add(numbers[i][j]);
+                    y.add(j);
+                }
+            }
+            for (int j = list.size() - 1; j >= 0; j--) {
+                numbers[i][indexY_last] = list.get(j);
+                indexY_last--;
+            }
+            indexY_last = Y_COUNT - 1;
+            ////如果两个相邻的瓷砖编号相同，它们将在停止移动后合并为一个瓷砖，其数值等于它们值的总和
+            if (list.size() > 1) {
+                for (int j = indexY_last; j > 0; j--) {
+                    if (numbers[i][j] == numbers[i][j - 1]) {
+                        numbers[i][j] = numbers[i][j] + numbers[i][j - 1];
+                        numbers[i][j - 1] = 0;
+                        //并且这个新合并的瓷砖也将沿着移动方向继续移动，直到不能再移动。
+                        for (int k = j - 1; k < indexY_last; k++) {
+                            numbers[i][k] = numbers[i][k + 1];
+                        }
+                        numbers[i][indexY_last] = 0;
+                    }
+                }
+            }else if (list.size() == 1){
+                numbers[i][indexY_last] = list.get(0);
+                //原来的位置要清零
+                numbers[i][y.get(0)] = 0;
+            }
+        }
+    }
+    public void moveLeft() {}
+    public void moveUp() {}
+    public void moveDown() {}
+
+
+    public void addRandomNumber() {//用于添加随机数字，需要加到上面的moveLeft()等方法最后
+        while (true) {
+            int X = random.nextInt(X_COUNT);
+            int Y = random.nextInt(Y_COUNT);
+            if (numbers[X][Y] == 0) {
+                int randomNum2 = random.nextInt(2);//生成0和1的随机整数,0就对应2，1就对应4
+                if (randomNum2 == 0) {
+                    numbers[X][Y] = 2;
+                } else {
+                    numbers[X][Y] = 4;
+                }
+                break;
+            }
         }
     }
 
