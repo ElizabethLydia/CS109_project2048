9d92096a1d04c5ad9639166a3a6b4d0454a55a6b
Lydia <1402640435@qq.com>
2024-05-09 17:10:03 +0800

    目前的撤销操作可以实现步数和分数的更新，但弹窗和游戏状态更新并为实现

---
+++ project/project-demo-src/src/util/RoundedPanel.java
@@ -0,0 +1,41 @@
+package util;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedPanel extends JPanel {
+    private Color backgroundColor;// 添加一个用于存储背景颜色的成员变量
+    private int x, y, width, height;
+    private Container parent;
+
+    // 为RoundedPanelExample类添加带参数的构造函数
+    public RoundedPanel(Color backgroundColor, int x, int y, int width, int height,Container parent) {
+        this.backgroundColor = backgroundColor;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.parent = parent;
+        setBounds(x, y, width, height);
+        setOpaque(false);
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g.create();
+
+        int arc = 20;
+        int width = getWidth() - 1;
+        int height = getHeight() - 1;
+
+        RoundRectangle2D round = new RoundRectangle2D.Float(0, 0, width, height, arc, arc);
+
+        // 使用传入的背景颜色
+        g2d.setColor(backgroundColor);
+        g2d.fill(round);
+
+        g2d.dispose();
+    }
+}
