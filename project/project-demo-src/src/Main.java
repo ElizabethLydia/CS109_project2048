506dd11fb68d0d4044e2b576467127ad7c215c04
Mesonlu <wang050109@126.com>
2024-05-14 16:17:52 +0800

    生日快乐

--- project/project-demo-src/src/Main.java
+++ project/project-demo-src/src/Main.java
@@ -1,12 +1,12 @@
-import view.GameFrame;
-import view.Menu;
+import view.Menu1;
+import view.Menu2;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            Menu menu = new Menu();
+            Menu1 menu = new Menu1();
             menu.setVisible(true);
             //要改变逻辑了先调出menu界面
             //fjy同学生日快乐！！
